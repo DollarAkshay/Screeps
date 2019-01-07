8de25e6a5d846e6fea02a969cf2c6a11860a9d59
DollarAkshay <akshay95aradhya@gmail.com>
2019-01-07 18:10:27 +0530

    Reduce Limit

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ module.exports.loop = function () {
         let upgraderCount = Game.spawns[SPAWN_NAME].memory['upgraderCount'];
         let harvesterCount = Game.spawns[SPAWN_NAME].memory['harvesterCount'];
 
-        if (upgraderCount < harvesterCount && upgraderCount < 6) {
+        if (upgraderCount < harvesterCount && upgraderCount < 3) {
             Game.spawns[SPAWN_NAME].spawnCreep([WORK, CARRY, MOVE], 'Upgrader_' + upgraderCount, {
                 memory: {role: 'upgrader'}
             });
