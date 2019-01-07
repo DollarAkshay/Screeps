8459dcdd93e93cccf6a7f1f520d924a64cd977e4
DollarAkshay <akshay95aradhya@gmail.com>
2019-01-07 18:25:27 +0530

    Resetting Builder Counter

--- gameHelpers.js
+++ gameHelpers.js
@@ -6,6 +6,7 @@ var SPAWN_NAME = GLOBAL.SPAWN_NAME;
 function resetCounters () {
     Game.spawns[SPAWN_NAME].memory['upgraderCount'] = 0;
     Game.spawns[SPAWN_NAME].memory['harvesterCount'] = 0;
+    Game.spawns[SPAWN_NAME].memory['builderCount'] = 0;
 }
 
 module.exports = {
