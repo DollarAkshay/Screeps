36060edbd9400cb074d210d7d4f7f8fb7c3ebb42
DollarAkshay <akshay95aradhya@gmail.com>
2019-01-07 18:20:39 +0530

    Adding builder commands

--- main.js
+++ main.js
@@ -17,14 +17,19 @@ module.exports.loop = function () {
         let upgraderCount = Game.spawns[SPAWN_NAME].memory['upgraderCount'];
         let harvesterCount = Game.spawns[SPAWN_NAME].memory['harvesterCount'];
 
-        if (upgraderCount < harvesterCount && upgraderCount < 3) {
+        if (Game.spawns[SPAWN_NAME].memory['harvesterCount'] < 6) {
+            Game.spawns[SPAWN_NAME].spawnCreep([WORK, CARRY, MOVE], 'Harvester_' + harvesterCount, {
+                memory: {role: 'harvester'}
+            });
+        }
+        else if (Game.spawns[SPAWN_NAME].memory['upgraderCount'] < 3) {
             Game.spawns[SPAWN_NAME].spawnCreep([WORK, CARRY, MOVE], 'Upgrader_' + upgraderCount, {
                 memory: {role: 'upgrader'}
             });
         }
-        else if (harvesterCount < 6) {
-            Game.spawns[SPAWN_NAME].spawnCreep([WORK, CARRY, MOVE], 'Harvester_' + harvesterCount, {
-                memory: {role: 'harvester'}
+        else if (Game.spawns[SPAWN_NAME].memory['builderCount'] < 3) {
+            Game.spawns[SPAWN_NAME].spawnCreep([WORK, CARRY, MOVE], 'Builder_' + upgraderCount, {
+                memory: {role: 'builder'}
             });
         }
     }
