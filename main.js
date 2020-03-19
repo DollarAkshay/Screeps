cd1d844301a9e4b66ca9d041de42a86ff2b45542
DollarAkshay <akshay95aradhya@gmail.com>
2020-03-19 12:12:59 +0530

    Bug Fix

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function calculateStage () {
         filter: {structureType: STRUCTURE_CONTAINER}
     }).length;
     let extensionCount = Game.spawns[SPAWN_NAME].room.find(FIND_STRUCTURES, {
-        filter: {structureType: STRUCTURE_CONTAINER}
+        filter: {structureType: STRUCTURE_EXTENSION}
     }).length;
 
     if (extensionCount >= 5 && containerCount >= 1) {
