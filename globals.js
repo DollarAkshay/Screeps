cc10be8561db127a08c68a7b4cc084696151b0d6
DollarAkshay <akshay95aradhya@gmail.com>
2020-03-19 08:45:27 +0530

    Adding hauler path visuals

--- globals.js
+++ globals.js
@@ -4,19 +4,23 @@ var GLOBAL = {
 
     'HARVESTER_PATH': {
         stroke: '#00dd33',
-        opacity: 0.3
+        opacity: 0.4
     },
     'UPGRADER_PATH': {
         stroke: '#1483f7',
-        opacity: 0.3
+        opacity: 0.4
     },
     'BUILDER_PATH': {
         stroke: '#ed0d33',
-        opacity: 0.3
+        opacity: 0.4
     },
     'REPAIRER_PATH': {
         stroke: '#f99409',
-        opacity: 0.3
+        opacity: 0.4
+    },
+    'HAULER_PATH': {
+        stroke: '#f9ee09',
+        opacity: 0.4
     }
 };
 
