fc751aa33d20657e030b5dfd870f7e1de2bf154a
DollarAkshay <akshay95aradhya@gmail.com>
2020-03-20 11:06:51 +0530

    Refil tower only if below 50%

--- unit_hauler.js
+++ unit_hauler.js
@@ -112,7 +112,7 @@ function bestTransferSource (creep) {
     // Towers
     let tower = creep.pos.findClosestByRange(FIND_STRUCTURES, {
         filter: (structure) => {
-            return structure.structureType === STRUCTURE_TOWER && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+            return structure.structureType === STRUCTURE_TOWER && structure.store.getFreeCapacity(RESOURCE_ENERGY) > structure.store.getCapacity(RESOURCE_ENERGY) / 2;
         }
     });
     if (tower != null) {
